8830647da2fc1195495491475d0d5898c693ed81
andriydobrosynets <andriy.andriy1995@gmail.com>
2021-11-18 04:09:02 +0200

    lesson1 HW

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule} from "@angular/common/http";
+import { UserComponent } from './components/user/user.component';
+import { UsersComponent } from './components/users/users.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { PostComponent } from './components/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    UsersComponent,
+    PostsComponent,
+    PostComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/post.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Post} from "../components/models/Post";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+
+  private url = "https://jsonplaceholder.typicode.com/posts"
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getPost(): Observable<Post[]> {
+    return this.httpClient.get<Post[]>(this.url)
+  }
+}
